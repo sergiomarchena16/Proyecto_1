709a80b25be8d61249030adb53be5db6ae6e2fb9
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2022-02-02 01:03:43 -0600

    updates
    updates

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-# Proyecto 1 - Deteccion de Ataques
+# 'Proyecto 1 - Deteccion de Ataques'
 # Sergio Marchena - 16387
-# Proyecto SIMARGL
-# Febrero 2022
+# Project: SIMARGL
+# February 2022
 
+# Librerias a utlizar
+import numpy as np
+import pandas as pd
+
+# Load the dataset
+df_part1 = pd.read_csv("D:\datasets\dataset-part1.csv", encoding='utf-8')
+df_part2 = pd.read_csv("D:\datasets\dataset-part2.csv", encoding='utf_8')
+
+df_part1.head()
